84f3243430d8311f4b85a6e5eab37c2dc0b73212
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-08-04 23:19:11 +0100

    Deployed 4b0169c with MkDocs version: 0.16.3

--- extra_javascript/my_editor.js
+++ extra_javascript/my_editor.js
@@ -1,27 +1,15 @@
-//function editor_from_id_and_string(id, string)
-//{
-//    CodeMirror.fromTextArea(id, {
-//        value: string,
-//        readOnly: true,
-//        // height: "350px",
-//        // parserfile: "parsexml.js",
-//        // stylesheet: "css/xmlcolors.css",
-//        // path: "js/",
-//        // continuousScanning: 500,
-//        lineNumbers: true
-//    });
-//}
-// See also http://jsfiddle.net/igos/qLAvN/
+/* That selector matches all spans that have an id attribute and it starts with foo (e.g. fooblah
+*/
+
 $(function() {
-    // That selector matches all spans that have an id attribute and it starts with foo (e.g. fooblah
-    $("div[id^=editor]").each(function() {
-        var editor = ace.edit(this.id);
-        //var editor = ace.edit("editor");
+    $(".editor").each(function(index, element) {
+        var editor = ace.edit(element.id);
         //editor.setTheme("ace/theme/monokai");
         //editor.setTheme("ace/theme/chrome");
         editor.setTheme("ace/theme/github");
         //editor.getSession().setMode("ace/mode/javascript");
-        //editor.setHighlightActiveLine(false);
+        editor.setHighlightActiveLine(true);
+        editor.setAutoScrollEditorIntoView(true);
         editor.setReadOnly(true);  // false to make it editable
     });
 });
