ec1799e210fdb508ac8a70f8d9470e9296eb04a8
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-08-20 16:22:57 +0100

    Deployed a443544 with MkDocs version: 0.16.3

--- extra_javascript/abidocs.js
+++ extra_javascript/abidocs.js
@@ -1,9 +1,10 @@
-/* That selector matches all spans that have an id attribute and it starts with foo (e.g. fooblah */
+
+// That selector matches all spans that have an id attribute and it starts with foo (e.g. fooblah
 $(function() {
     $(".editor").each(function(index, element) {
+        element.removeAttribute("hidden")
         var editor = ace.edit(element.id);
         //editor.setTheme("ace/theme/monokai");
-        //editor.setTheme("ace/theme/chrome");
         editor.setTheme("ace/theme/github");
         //editor.getSession().setMode("ace/mode/javascript");
         editor.setHighlightActiveLine(true);
@@ -11,6 +12,7 @@ $(function() {
         editor.setReadOnly(true);  // false to make it editable
     });
 });
+
 
 // https://github.com/mkdocs/mkdocs/wiki/MkDocs-Recipes#associate-github-page-with-current-mkdoc-page
 $(function() {
@@ -25,3 +27,24 @@ $(function() {
     a_git = $('[href="https://github.com/abinit/abinit"]');
     a_git.attr('href', url).attr('target', '_blank');
 })
+
+// The following code will enable all popovers in the document:
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+});
+
+
+// Return to top arrow. https://codepen.io/rdallaire/pen/apoyx
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0                       // Scroll to top of body
+    }, 500);
+});
