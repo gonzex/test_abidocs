85f8a761c34954d6882f616c302e9136e041d798
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-10-05 13:42:53 +0100

    Deployed 0d55c7e with MkDocs version: 0.16.3

--- extra_javascript/abidocs.js
+++ extra_javascript/abidocs.js
@@ -17,6 +17,20 @@ $(function() {
         }, 500);
     });
 
+    $('.modal-dialog').draggable().resizable();
+
+    //$(".my-dialog").dialog({
+    //    autoOpen: false,
+    //    buttons: [
+    //        {text: "Ok",
+    //         icon: "ui-icon-heart",
+    //         click: function() {
+    //            $(this).dialog("close");
+    //        }
+    //    }]
+    //});
+    //$("#foobar").dialog();
+
     // That selector matches all spans that have an id attribute and it starts with foo (e.g. fooblah
     $(".editor").each(function(index, element) {
         element.removeAttribute("hidden")
@@ -58,90 +72,6 @@ $(window).scroll(function() {
     }
 });
 */
-
-// https://facelessuser.github.io/pymdown-extensions/extensions/details/
-(function () {
-'use strict';
-/**
- * Converts details/summary tags into working elements in browsers that don't yet support them.
- * @return {void}
- */
-var details = (function () {
-
-  var isDetailsSupported = function () {
-    // https://mathiasbynens.be/notes/html5-details-jquery#comment-35
-    // Detect if details is supported in the browser
-    var el = document.createElement("details");
-    var fake = false;
-
-    if (!("open" in el)) {
-      return false;
-    }
-
-    var root = document.body || function () {
-      var de = document.documentElement;
-      fake = true;
-      return de.insertBefore(document.createElement("body"), de.firstElementChild || de.firstChild);
-    }();
-
-    el.innerHTML = "<summary>a</summary>b";
-    el.style.display = "block";
-    root.appendChild(el);
-    var diff = el.offsetHeight;
-    el.open = true;
-    diff = diff !== el.offsetHeight;
-    root.removeChild(el);
-
-    if (fake) {
-      root.parentNode.removeChild(root);
-    }
-
-    return diff;
-  }();
-
-  if (!isDetailsSupported) {
-    var blocks = document.querySelectorAll("details>summary");
-    for (var i = 0; i < blocks.length; i++) {
-      var summary = blocks[i];
-      var details = summary.parentNode;
-
-      // Apply "no-details" to for unsupported details tags
-      if (!details.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
-        details.className += " no-details";
-      }
-
-      summary.addEventListener("click", function (e) {
-        var node = e.target.parentNode;
-        if (node.hasAttribute("open")) {
-          node.removeAttribute("open");
-        } else {
-          node.setAttribute("open", "open");
-        }
-      });
-    }
-  }
-});
-
-(function () {
-  var onReady = function onReady(fn) {
-    if (document.addEventListener) {
-      document.addEventListener("DOMContentLoaded", fn);
-    } else {
-      document.attachEvent("onreadystatechange", function () {
-        if (document.readyState === "interactive") {
-          fn();
-        }
-      });
-    }
-  };
-
-  onReady(function () {
-    details();
-  });
-})();
-
-}());
-
 
 // This for the table of variables implemented by Jordan
 
